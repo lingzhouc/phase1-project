aedd0589664a1808d5d391f0864a2145516f4eb7
LingZhouChen <lingzhouc@gmail.com>
2023-09-27 10:42:21 -0400

    styled search bar and select options

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const searchForm = document.querySelector('#search-form')
 const resultsDiv = document.querySelector('#drink-results')
+const drinkDiv = document.querySelector("#selected-drink")
+const sortField = document.querySelector('#sort-by')
+
+sortField.disabled = true
+sortField.selectedIndex = 0
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    sortField.disabled = false
+    sortField.selectedIndex = 0
     resultsDiv.innerHTML = ''
-    console.log(e.target['search-option'].value)
-    console.log(e.target['search-bar'].value)
     search(e.target['search-option'], e.target['search-bar'].value)
-
-    e.target.reset
 })
+
+sortField.addEventListener('change', sortCards)
 
 function search(searchOption, searchValue){
     const searchValueWithPlus = searchValue.replace(/ /g, "+")
     const searchUrl = urlIdentifier(searchOption) + searchValueWithPlus 
-    console.log(searchUrl)
     fetch(searchUrl)
     .then(r => {
         if(r.ok) {
@@ -23,17 +27,19 @@ function search(searchOption, searchValue){
             throw r.statusText
         }
     })
-    .then(result => { console.log(result)
+    .then(result => {
         const drinksArr = result.drinks
-        console.log(drinksArr)
         if (drinksArr !== null){
             drinksArr.forEach(createDrinkCard)
+            
         }else{
-            resultsDiv.textContent = 'No drinks found. Try again!'
+            throw('none')
         }
-    }).catch(error => resultsDiv.textContent = 'No drinks found. Try again!')
-    }
-
+    }).catch(error => {
+        sortField.disabled = true
+        resultsDiv.textContent = 'No drinks found. Try again!'
+    })
+}
 
 function urlIdentifier(searchOption){
     if(searchOption.value === 'drinkName'){
@@ -53,8 +59,80 @@ function createDrinkCard(drink){
     drinkImg.className = 'drink-image'
     drinkImg.width = 100
     drinkImg.height = 100
-    console.log(drinkImg)
     drinkImg.src = drink.strDrinkThumb
+    drinkImg.alt = drink.strDrink
     drinkCard.append(drinkImg, drinkName)
     resultsDiv.append(drinkCard)
+
+    let category = ''
+    let glass = ''
+    let drinkInstructions = ''
+    let alcoholic = ''
+    let ingredient = ''
+
+        // click drink img to see info
+    if(!drink.strCategory){
+        drinkId = drink.idDrink
+        drinkImg.addEventListener("click", e=>{
+            fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+drinkId)
+            .then(r=> r.json())
+            .then(response =>{
+            const newDrink = response.drinks[0]
+            setDetails(newDrink)
+            populateDetails(e)})
+        })
+    }else{
+        setDetails(drink)
+        drinkImg.addEventListener("click", populateDetails)
+    }
+    
+    function setDetails(drink){
+        category = drink.strCategory
+        glass = drink.strGlass 
+        drinkInstructions = drink.strInstructions
+        if (drink.strAlcoholic === "Alcoholic") {
+            alcoholic = "Yes"
+        } else {
+            alcoholic = "No"
+        }
+        for (let i = 1; i < 16; i++) {
+        if (!drink["strIngredient"+ i]) break;
+        ingredient += `${drink["strMeasure"+ i]} ${drink["strIngredient"+ i]} <br>`;
+        }
+    }
+
+    function populateDetails(event){
+        drinkDiv.innerHTML = `
+            <h2>${drinkName.textContent}</h1>
+            <img src="${drinkImg.src}">
+            <p>Category: ${category}</p>
+            <p>Alcoholic: ${alcoholic}</p>
+            <p>Glass: ${glass}</p>
+            <h3>Ingredients</h3>
+            <p>${ingredient}</p>
+            <h3>Instructions</h3>
+            <p>${drinkInstructions}</p>
+        `
+    }
 }
+
+function sortCards(sortEvent){
+
+    const drinkCards = Array.from(document.querySelectorAll('.drink-card'))
+    let sortedDrinkCards = []
+    if (sortEvent.target.value === 'a-to-z'){
+        sortedDrinkCards = drinkCards.sort((a,b)=>{
+            return a.querySelector('p').textContent.localeCompare(b.querySelector('p').textContent)
+        })
+    }else if (sortEvent.target.value === 'z-to-a'){
+        sortedDrinkCards = drinkCards.sort((a,b)=>{
+            return a.querySelector('p').textContent.localeCompare(b.querySelector('p').textContent)
+        }).reverse()
+    } else {
+        
+    }
+    for (card of sortedDrinkCards){
+        resultsDiv.append(card)
+    }
+}
+
